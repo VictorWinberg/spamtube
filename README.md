ace1236afbc3b0c92f16ef999b7d5290bd1cf1e8
Victor Winberg <victor.winberg@doktor.se>
2022-08-15 21:49:34 +0200

    feat(db): add initial pg-database

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 - [Google Drive](https://drive.google.com/drive/folders/1x6eskGhW1XJcFSHURoiRQhkAj4kD99N0)
 - [Slack](https://theboardgamesgroup.slack.com/archives/C03PF2S0PFG)
 - [Keybase](https://keybase.io/team/spamtube)
+
+## Setup
+
+### Docker
+Initiate a new docker container:
+
+```bash
+docker compose up --build
+```
+
+## Development
+
+### Database
+Connect to the database with the following command:
+```bash
+pgcli postgres://postgres:postgres@localhost:5337/spamtube_db
+```
+
+> Note: To run pgcli you need to install it from https://www.pgcli.com/install
+
+Run database migrations manually with the following command:
+```bash
+cd database
+go run main.go -dsn "postgres://postgres:postgres@localhost:5337/spamtube_db?sslmode=disable"
+```
---
+++ database/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"embed"
+	"flag"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+func main() {
+	var db *sql.DB
+	dsn := flag.String("dsn", os.Getenv("DATABASE_URL"), "Postgres data source name")
+	flag.Parse()
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		panic(err)
+	}
+
+	if err := goose.Up(db, "migrations"); err != nil {
+		panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
