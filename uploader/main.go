92db26055819ff4881863226faa9281f863d13e8
antonidag <antonidag@hotmail.com>
2022-08-16 01:09:12 +0200

    add somekind of oauth flow, needs to be improved

--- uploader/main.go
+++ uploader/main.go
@@ -11,10 +11,10 @@ var (
 	apikey      = flag.String("apikey", "", "Youtube API Key")
 	filename    = flag.String("filename", "", "Name of video file to upload")
 	title       = flag.String("title", "", "Video title")
-	description = "Test Description"
-	category    = "22"
-	keywords    = "spamtube"
-	privacy     = "listed"
+	description = flag.String("description", "This is video was made by the SpamTube Team", "Description of the video")
+	category    = flag.String("category", "22", "Category")
+	keywords    = flag.String("keywords", "spamtube,ai,fyp", "Keyword for search")
+	privacy     = "public"
 )
 
 func main() {
@@ -28,8 +28,12 @@ func main() {
 		panic("You must provide a filename of a video file to upload")
 	}
 
+	if *title == "" {
+		panic("You must provide a title")
+	}
+
 	fmt.Println("Upload file " + *filename)
-	id, err := yt.UploadToYoutube(*apikey, *filename, *title, description, category, privacy, keywords)
+	id, err := yt.UploadToYoutube(*apikey, *filename, *title+"#shorts", *description, *category, privacy, *keywords)
 	if err != nil {
 		log.Fatalln(err)
 		return
