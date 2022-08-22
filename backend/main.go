ae7ee735ff453d390e6ee3b9e25527d27b82074f
Victor Winberg <victor.winberg@doktor.se>
2022-08-22 22:58:27 +0200

    feat(backend): initial backend (dockerized)

---
+++ backend/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	port := ":3000"
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("../frontend/dist", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	// Start and run the server
+	router.Run(port)
+}
