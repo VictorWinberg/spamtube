59c88200d9c10c18e78862d64a17da95ea8223eb
Victor Winberg <victor.winberg@doktor.se>
2022-09-19 23:03:31 +0200

    fix typo

--- uploader/oauth2.go
+++ uploader/oauth2.go
@@ -59,7 +59,7 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 func getClient(scope ...string) *http.Client {
 	ctx := context.Background()
 
-	b, err := ioutil.ReadFile("client_secret.json")
+	b, err := ioutil.ReadFile("client_secrets.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
