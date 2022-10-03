4d6e4d76cba1d31fb06acfa71e81ae3c3dbc5e36
Victor Winberg <victor.winberg@doktor.se>
2022-10-03 20:37:33 +0200

    only free music

--- music-maker/run.js
+++ music-maker/run.js
@@ -44,10 +44,10 @@ function randomIntFromInterval(min, max) {
 }
 
 async function main() {
-  const page = randomIntFromInterval(1, 25);
+  const page = randomIntFromInterval(1, 5);
   
-  console.log(`Getting a popular song from https://www.bensound.com/royalty-free-music/${page}...`);
-  const htmlstr = await fetchAsync(`https://www.bensound.com/royalty-free-music/${page}`);
+  console.log(`Getting a popular song from https://www.bensound.com/free-music-for-videos/${page}...`);
+  const htmlstr = await fetchAsync(`https://www.bensound.com/free-music-for-videos/${page}`);
   const songNames = getSongNames(htmlstr, ".result-container > div:first-child > div > .track-name a span");
 
   console.log("Pool of songs:", { array: songNames });
