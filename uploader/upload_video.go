0be8fa1ebc57caa8c013cc9e50eb2aa25334984d
Victor Winberg <victor.winberg@doktor.se>
2022-10-03 20:29:08 +0200

    fix getenv

--- uploader/upload_video.go
+++ uploader/upload_video.go
@@ -70,8 +70,8 @@ func main() {
 }
 
 func getEnv(key, fallback string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
+	value := os.Getenv(key)
+	if value == "" {
 		value = fallback
 	}
 	return value
