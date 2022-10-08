084312e2a66c2455574bfca9ce1a783fc8121862
Victor Winberg <victor.m.winberg@gmail.com>
2022-10-08 09:28:25 +0200

    Update environment.go

--- backend/helpers/environment.go
+++ backend/helpers/environment.go
@@ -2,9 +2,9 @@ package helpers
 
 import "os"
 
-func GetEnv(key, fallback string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
 		value = fallback
 	}
 	return value
