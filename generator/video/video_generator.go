e23c4c8a3bc511e74dc0452d596945d6429d0c83
Anton Göransson <anton.goransson95@gmail.com>
2022-10-17 18:50:43 +0200

    feat(video-generator) Reduce music volume (#30)
    * Reduce music volume
    
    * Rename volume constant

--- generator/video/video_generator.go
+++ generator/video/video_generator.go
@@ -2,8 +2,11 @@ package generator
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
@@ -18,19 +21,22 @@ const OUTPUT_VIDEO_FORMAT = "yuvj420p"
 const VIDEO_CODEC = "libx264"
 const OUTPUT_FPS = 30
 const MIN_AUDIO_FILES = 2
+const VOLUME_CHANGE_FACTOR = "0.5"
 
 func CreateVideo() {
 	imagePath, imageExt := GetImages()
 	audio := GenerateAudio()
-	err := GenerateVideo(imagePath, imageExt, audio)
-	fmt.Println(err)
+	GenerateVideo(imagePath, imageExt, audio)
+
 }
 
-func GenerateVideo(imagePath string, imageExt string, audioInput *ffmpeg.Stream) error {
+func GenerateVideo(imagePath string, imageExt string, audioInput *ffmpeg.Stream) {
 	imageInput := ffmpeg.Input(imagePath+"%03d."+imageExt, ffmpeg.KwArgs{"loop": 1, "framerate": "1/2"})
-
 	err := ffmpeg.Concat([]*ffmpeg.Stream{imageInput, audioInput}, ffmpeg.KwArgs{"v": 1, "a": 1}).Output(OUTPUT_VIDEO_FILE, ffmpeg.KwArgs{"r": OUTPUT_FPS, "pix_fmt": OUTPUT_VIDEO_FORMAT, "t": VIDEO_LENGTH, "c:v": VIDEO_CODEC}).OverWriteOutput().ErrorToStdOut().Run()
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully generated video: " + OUTPUT_VIDEO_FILE)
 }
 
 func GenerateAudio() *ffmpeg.Stream {
@@ -39,14 +45,12 @@ func GenerateAudio() *ffmpeg.Stream {
 	if nFoundFiles < MIN_AUDIO_FILES {
 		log.Fatalf("Expected at least %d audiofiles, found %d", MIN_AUDIO_FILES, nFoundFiles)
 	}
-	audioInputs := []*ffmpeg.Stream{}
-	for path, audioFiles := range pathFiles {
-		for _, audioFile := range audioFiles {
-			audioInputs = append(audioInputs, ffmpeg.Input(path+audioFile.Name()))
-		}
-	}
+	audioInputs := GetAsFfmpegAudioInputs(pathFiles)
 	err := ffmpeg.Filter(audioInputs, "amix", ffmpeg.Args{fmt.Sprintf("inputs=%d", nFoundFiles), "duration=longest"}).Output(OUTPUT_AUDIO_FILE, ffmpeg.KwArgs{"c:a": "libmp3lame", "t": 60}).OverWriteOutput().ErrorToStdOut().Run()
-	fmt.Println(err)
+	if err != nil {
+		fmt.Println("Failed to generate audio for video")
+		log.Fatal(err)
+	}
 	return ffmpeg.Input(OUTPUT_AUDIO_FILE)
 }
 
@@ -94,6 +98,34 @@ func GetAudio() map[string][]os.DirEntry {
 		}
 	}
 	return pathFiles
+}
+
+func GetAsFfmpegAudioInputs(pathFiles map[string][]os.DirEntry) []*ffmpeg.Stream {
+	audioInputs := []*ffmpeg.Stream{}
+	for path, audioFiles := range pathFiles {
+		for _, audioFile := range audioFiles {
+			if strings.Contains(path, "music") {
+				reducedAudioFile := ReduceVolume(path, audioFile)
+				audioInputs = append(audioInputs, reducedAudioFile)
+			} else {
+				audioInputs = append(audioInputs, ffmpeg.Input(path+audioFile.Name()))
+			}
+			fmt.Println("Found audio " + audioFile.Name())
+		}
+	}
+	return audioInputs
+}
+
+func ReduceVolume(path string, audioFile fs.DirEntry) *ffmpeg.Stream {
+	fmt.Println("Trying to reduce volume of file: " + path + audioFile.Name())
+	outPutFile := "out/" + audioFile.Name()
+	err := ffmpeg.Input(path+audioFile.Name()).Filter("volume", ffmpeg.Args{VOLUME_CHANGE_FACTOR}).Output(outPutFile).OverWriteOutput().Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	d, _ := strconv.ParseFloat(VOLUME_CHANGE_FACTOR, 32)
+	fmt.Printf("Reduced volume to %d%% of file: %s as: %s\n", int(d*100), path+audioFile.Name(), outPutFile)
+	return ffmpeg.Input(outPutFile)
 }
 
 func ExtractFilesWithExt(files []os.DirEntry, searchFileExt string) []os.DirEntry {
