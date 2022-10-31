3f3528d1d185a4bfc6916a410cc8a62992248d36
Victor Winberg <victor.m.winberg@gmail.com>
2022-10-31 19:36:41 +0100

    (backend): working youtube serach api

--- backend/api/youtube.go
+++ backend/api/youtube.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/youtube/v3"
@@ -16,11 +17,25 @@ func GetUploadedYoutubeVideos() gin.HandlerFunc {
 		if err != nil {
 			panic(fmt.Sprintf("Error creating YouTube client: %v", err))
 		}
-		channelResponse, err := service.Search.List([]string{"snippet"}).Type("video").MaxResults(100).ForMine(true).Do()
-		if err != nil {
-			con.JSON(http.StatusInternalServerError, err)
+		var youtubeItems []*youtube.SearchResult
+
+		toDateTime := time.Unix(time.Now().Unix(), 0).Format(time.RFC3339)
+		fromDateTime := time.Unix(0, 0).Format(time.RFC3339) //carbon.Parse("1970-01-01").ToRfc3339String()
+		for true {
+			channelResponse, err := service.Search.List([]string{"snippet"}).Type("video").MaxResults(50).PublishedAfter(fromDateTime).PublishedBefore(toDateTime).ForMine(true).Do()
+			if err != nil {
+				con.JSON(http.StatusInternalServerError, err)
+			}
+			youtubeItems = append(youtubeItems, channelResponse.Items...)
+
+			fromDateTime = *&channelResponse.Items[0].Snippet.PublishedAt
+			if fromDateTime == "" {
+				break
+			}
+			break
 		}
-		con.JSON(http.StatusOK, channelResponse.Items)
+
+		con.JSON(http.StatusOK, youtubeItems)
 	}
 
 	return gin.HandlerFunc(fn)
