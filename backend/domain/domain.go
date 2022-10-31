2cac4cfff009e0f3f63f5072c5e04b08bd6a05b8
Jan Zubac <jan.zubac@devoteam.com>
2022-10-31 19:59:14 +0100

    feat: keywords in reddit post data

--- backend/domain/domain.go
+++ backend/domain/domain.go
@@ -15,23 +15,24 @@ type RedditResponseTop struct {
 	Data struct {
 		Children []struct {
 			Data struct {
-				Subreddit         string  `json:"subreddit"`
-				Selftext          string  `json:"selftext"`
-				AuthorFullname    string  `json:"author_fullname"`
-				Title             string  `json:"title"`
-				UpvoteRatio       float64 `json:"upvote_ratio"`
-				Ups               int     `json:"ups"`
-				IsOriginalContent bool    `json:"is_original_content"`
-				Score             int     `json:"score"`
-				Thumbnail         string  `json:"thumbnail"`
-				Created           float64 `json:"created"`
-				Over18            bool    `json:"over_18"`
-				SubredditID       string  `json:"subreddit_id"`
-				ID                string  `json:"id"`
-				Author            string  `json:"author"`
-				Permalink         string  `json:"permalink"`
-				URL               string  `json:"url"`
-				CreatedUtc        float64 `json:"created_utc"`
+				Subreddit         string   `json:"subreddit"`
+				Selftext          string   `json:"selftext"`
+				AuthorFullname    string   `json:"author_fullname"`
+				Title             string   `json:"title"`
+				UpvoteRatio       float64  `json:"upvote_ratio"`
+				Ups               int      `json:"ups"`
+				IsOriginalContent bool     `json:"is_original_content"`
+				Score             int      `json:"score"`
+				Thumbnail         string   `json:"thumbnail"`
+				Created           float64  `json:"created"`
+				Over18            bool     `json:"over_18"`
+				SubredditID       string   `json:"subreddit_id"`
+				ID                string   `json:"id"`
+				Author            string   `json:"author"`
+				Permalink         string   `json:"permalink"`
+				URL               string   `json:"url"`
+				CreatedUtc        float64  `json:"created_utc"`
+				Keywords          []string `json:"keywords"`
 			} `json:"data"`
 		} `json:"children"`
 	} `json:"data"`
---
+++ backend/keywords/keywords.go
@@ -0,0 +1,95 @@
+package keywords
+
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+// WordCount holds word and count pair
+type WordCount struct {
+	word  string
+	count int
+}
+
+// Extract Extract keywords from a string.
+// Inspired by https://github.com/securisec/go-keywords/blob/master/keywords.go
+func Extract(s string) ([]string, error) {
+	var (
+		results      []string
+		words        []string
+		matchWords   []string
+		strippedHTML string
+	)
+
+	strippedHTML = s
+
+	splitRe := regexp.MustCompile(`\s`)
+	words = splitRe.Split(strings.TrimSpace(strippedHTML), -1)
+
+	if len(words) == 0 {
+		return catchErr(nil)
+	}
+
+	specialChars := "\\.|,|;|!|\\?|\\(|\\)|:|\"|\\^'|\\$|“|”|‘|’|”|<|>|–"
+
+	for _, w := range words {
+		w = regexp.MustCompile(specialChars).ReplaceAllString(w, "")
+		if len(w) == 1 {
+			w = regexp.MustCompile(`\-|_|@|&|#`).ReplaceAllString(w, "")
+		}
+		matchWords = append(matchWords, w)
+	}
+
+	for _, word := range matchWords {
+		if matcher(strings.ToLower(word)) {
+			results = append(results, strings.ToLower(word))
+		}
+	}
+
+	results = extractMostPopularKeywords(results, 10)
+
+	return results, nil
+}
+
+func catchErr(err error) ([]string, error) {
+	return []string{}, err
+}
+
+func matcher(s string) bool {
+	for _, w := range EnglishKeyWords {
+		if w == s {
+			return false
+		}
+	}
+	return true
+}
+
+func extractMostPopularKeywords(results []string, numberOfKeyWords int) []string {
+	// count same words in s
+	m := make(map[string]int)
+	for _, word := range results {
+		m[word]++
+	}
+
+	// create and fill slice of word-count pairs for sorting by count
+	wordCounts := make([]WordCount, 0, len(m))
+	for key, val := range m {
+		wordCounts = append(wordCounts, WordCount{word: key, count: val})
+	}
+
+	// sort wordCount slice by decreasing count number
+	sort.Slice(wordCounts, func(i, j int) bool {
+		return wordCounts[i].count > wordCounts[j].count
+	})
+
+	maxIndex := math.Min(float64(numberOfKeyWords), float64(len(wordCounts)))
+	var keywords []string
+
+	for i := 0; i < int(maxIndex); i++ {
+		keywords = append(keywords, wordCounts[i].word)
+	}
+
+	return keywords
+}
