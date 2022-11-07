328f331cf6d8800cd46767637cffe10a172a6fea
Victor Winberg <victor.m.winberg@gmail.com>
2022-11-07 19:36:26 +0100

    youtube web-scraper

--- backend/api/youtube.go
+++ backend/api/youtube.go
@@ -1,37 +1,56 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"os"
+	"unicode/utf8"
 
+	"strings"
+
+	"github.com/anaskhan96/soup"
 	"github.com/gin-gonic/gin"
-	"github.com/gocolly/colly"
 )
 
 func GetUploadedYoutubeVideos() gin.HandlerFunc {
 	fn := func(con *gin.Context) {
-		var array []*string
-		c := colly.NewCollector(
-			colly.AllowedDomains("books.toscrape.com"),
-		)
-		// https://oxylabs.io/blog/golang-web-scraper
-		c.OnHTML("title", func(e *colly.HTMLElement) {
-			fmt.Println(e.Text)
-			array = append(array, &e.Text)
-		})
+		resp, err := soup.Get("https://www.youtube.com/channel/UCTIp7LYLKOA6zq_PT21_NgA/shorts")
+		if err != nil {
+			os.Exit(1)
+		}
+		doc := soup.HTMLParse(resp)
+		links := doc.FindAll("script")
+		for _, link := range links {
 
-		c.OnResponse(func(r *colly.Response) {
-			fmt.Println(r.StatusCode)
-		})
+			var isFound = strings.Contains(link.HTML(), "GFEEDBACK")
+			if isFound {
+				var s = strings.ReplaceAll(link.HTML(), "<script nonce=", "") // >var ytInitialData =
+				var s1 = strings.ReplaceAll(s, ">var ytInitialData = ", "")   // ;</script>"""
+				var s2 = strings.ReplaceAll(s1, ";</script>", "")
+				var s3 = s2[24:len(s2)]
+				var s4 = trimLastChar(s3)
+				var s5 = trimLastChar(s4)
+				var s6 = trimLastChar(s5)
+				var s7 = strings.Trim(s6, "\"")
+				var jsonMap interface{}
+				err := json.Unmarshal([]byte(s7), &jsonMap)
+				if err != nil {
+					con.JSON(http.StatusOK, err)
+				}
+				//var s4 = s3[1 : len(s3)-2]
+				con.JSON(http.StatusOK, jsonMap)
+			}
 
-		c.OnRequest(func(r *colly.Request) {
-			fmt.Println("Visiting", r.URL)
-		})
-
-		c.Visit("https://books.toscrape.com/")
-
-		con.JSON(http.StatusOK, array)
+		}
+		con.JSON(http.StatusOK, "")
 	}
 
 	return gin.HandlerFunc(fn)
+}
+func trimLastChar(s string) string {
+	r, size := utf8.DecodeLastRuneInString(s)
+	if r == utf8.RuneError && (size == 0 || size == 1) {
+		size = 0
+	}
+	return s[:len(s)-size]
 }
--- backend/go.mod
+++ backend/go.mod
@@ -33,6 +33,7 @@ require (
 )
 
 require (
+	github.com/anaskhan96/soup v1.2.5
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
