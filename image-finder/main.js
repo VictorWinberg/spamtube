341de59566b68aefce0ddcc7212c07567e4c6ec2
Victor Winberg <victor.m.winberg@gmail.com>
2023-02-21 13:13:09 +0100

    fix(image-finder): resize images (#67)

--- image-finder/main.js
+++ image-finder/main.js
@@ -3,28 +3,34 @@ import fs from "fs";
 import nodeFetch from "node-fetch";
 import http from "node:https";
 import path from "path";
+import sharp from "sharp";
 import { createApi } from "unsplash-js";
 
 config({ path: path.resolve(".env.local"), override: true });
+
+const IMAGE_WIDTH = 1080;
+const IMAGE_HEIGHT = 1620;
 
 const unsplash = createApi({
   accessKey: process.env.UNSPLASH_ACCESS_TOKEN,
   fetch: nodeFetch,
 });
 
-function download(url, dest, cb) {
-  const file = fs.createWriteStream(dest);
-  http
-    .get(url, function (response) {
-      response.pipe(file);
-      file.on("finish", function () {
-        file.close(cb);
+async function download(url, dest) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    http
+      .get(url, function (response) {
+        response.pipe(file);
+        file.on("finish", function () {
+          file.close(() => resolve("done"));
+        });
+      })
+      .on("error", function (err) {
+        fs.unlink(dest);
+        reject(err.message);
       });
-    })
-    .on("error", function (err) {
-      fs.unlink(dest);
-      if (cb) cb(err.message);
-    });
+  });
 }
 
 async function main() {
@@ -48,12 +54,24 @@ async function main() {
 
   const photos = response.map((response) => response.response.results).flat();
   const links = photos.map((res) => res.urls.regular);
-  links.forEach((link, index) => {
-    const filename = String(index).padStart(3, "0");
-    download(link, `./out/${filename}.jpg`, () => {
-      console.log(`image ${index} downloaded`);
-    });
-  });
+  const downloads = await Promise.all(
+    links.map(async (link, index) => {
+      const filename = String(index).padStart(3, "0");
+      const res = await download(link, `./out/${filename}.jpg`);
+      console.log(`image ${index} downloaded`, res);
+      return `./out/${filename}.jpg`;
+    })
+  );
+
+  await Promise.all(
+    downloads.map(async (filename, index) => {
+      const buffer = await sharp(filename).resize(IMAGE_WIDTH, IMAGE_HEIGHT).toBuffer();
+      await sharp(buffer).toFile(filename);
+      console.log(`resize ${index}`);
+    })
+  );
+
+  console.log("Done!");
 }
 
 main();
