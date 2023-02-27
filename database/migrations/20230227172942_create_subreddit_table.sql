0026554caae767186d728e27b5daa18bedf8e9b1
antongoransson <anton.goransson95@gmail.com>
2023-02-27 18:14:01 +0100

    Add subreddits migrations cript

---
+++ database/migrations/20230227172942_create_subreddit_table.sql
@@ -0,0 +1,30 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE subreddits (
+  id        UUID DEFAULT  uuid_generate_v4(),
+  name      VARCHAR(255)  NOT NULL UNIQUE,
+
+  created_at TIMESTAMP DEFAULT NOW(),
+
+  PRIMARY KEY (id)
+);
+
+INSERT INTO subreddits(name) 
+VALUES
+('lifeofnorman'),
+('talesfromtechsupport'),
+('talesfromretail'),
+('glitch_in_the_matrix'),
+('no_sleep'),
+('UnresolvedMysteries'),
+('shortscarystories'),
+('psycho_alpaca'),
+('relationships')
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE subreddits;
+-- +goose StatementEnd
