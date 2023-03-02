d1d04f8025e1b55f40eb77ca7bee749d5c304ae9
Victor Winberg <victor.m.winberg@gmail.com>
2023-03-02 15:49:23 +0100

    Update chatgpt.py

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -12,8 +12,8 @@ chatbot = Chatbot(config={
 
 voice_input = os.getenv('VOICE_INPUT')
 
-prompt_title = "Please provide a clickbait title for the following text with a maximum of 42 characters: " + voice_input
-prompt_summarize = "Please summarize the following text into 40 seconds, but using the first person point of view: " + voice_input
+prompt_title = "Please provide a clickbait and dramatic title for the following text with a maximum of 42 characters: " + voice_input
+prompt_summarize = "Please summarize the following text into 40 seconds as exciting as possible to get the reader's attention, using the first person point of view, meaning using the pronouns I, me, we, and us, in order to tell a story from the narrator's perspective: " + voice_input
 
 for data in chatbot.ask(prompt_title):
     title = data["message"]
