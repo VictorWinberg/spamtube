fc66348e62bd70c857dbf6ee328143c9a50056bd
Jan Zubac <jan.zubac@devoteam.com>
2023-03-29 18:29:23 +0200

    actually add frontend types

---
+++ frontend/src/api/config.ts
@@ -0,0 +1,26 @@
+import { get, put, remove } from "./apiClient";
+
+export type ConfigEntry = {
+  id: string;
+  name: string;
+  createdAt: string;
+  cron: string;
+};
+
+export type ConfigEntryRequestBody = {
+  id?: string;
+  name?: string;
+  cron?: string;
+};
+
+export async function upsertConfig(body: ConfigEntryRequestBody) {
+  return put<ConfigEntry>("subreddits", JSON.stringify(body));
+}
+
+export async function deleteConfigEntry(id: string) {
+  return remove<string>(`delete/${id}`);
+}
+
+export async function getConfigurations() {
+  return get<ConfigEntry[]>("subreddits");
+}
