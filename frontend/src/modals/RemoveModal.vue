c94928c27552054f68ec95da100ae4d8abb70d46
AnnieLeonia <annie.onnered@gmail.com>
2023-04-01 11:04:00 +0200

    feat(modal): create remove modal component (#92)

---
+++ frontend/src/modals/RemoveModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog width="auto">
+    <v-card>
+      <v-card-text> Are you sure you want to remove this item? </v-card-text>
+      <v-card-actions>
+        <v-btn color="grey" @click="$emit('cancel')"> No </v-btn>
+        <v-btn color="error" @click="$emit('remove')"> Yes </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RemoveModal",
+  emits: ["cancel", "remove"],
+});
+</script>
+
+<style scoped lang="scss">
+.v-card-actions {
+  display: flex;
+  justify-content: space-evenly;
+
+  .v-btn {
+    margin: 0;
+  }
+}
+</style>
