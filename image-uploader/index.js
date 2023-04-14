01452ada1070e36b704a7c631a6d87e39e50d511
Anton Björkman <anton.bjorkman@enfogroup.com>
2023-04-14 12:06:25 +0200

    remove db connection

--- image-uploader/index.js
+++ image-uploader/index.js
@@ -2,9 +2,7 @@ import fs from 'fs'
 import path from "path";
 import request from 'request';
 import isImage from 'is-image';
-import prep from 'pg-prepared';
 import { config } from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { Client } from 'pg';
 
 config({ path: path.resolve(".env.local"), override: true });
 console.log("Saving images to online image database")
@@ -30,12 +28,6 @@ async function main() {
     base64ImageArray.push({ base64: base64String, name: file });
   }
   // Upload to db 
-  const DATABASE_CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING
-  const client = new Client({
-    connectionString: DATABASE_CONNECTION_STRING
-  })
-
-  await client.connect()
   for (const item of base64ImageArray) {
 
     let options = {
@@ -51,13 +43,6 @@ async function main() {
     request(options, async function (error, response) {
 
       if (error) throw new Error(error);
-      // First prepare statement without binding parameters
-      var item = prep('INSERT INTO img (url) VALUES (${url})')
-      // Then execute the query and bind parameters in loop
-      client.query(item({ url: response.body.data.display_url }), function (err, result) {
-        if (err) throw new Error(err)
-      })
-      await client.end()// will this work?
     });
   }
 }
