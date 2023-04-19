fdf334f419ff9e6faece3dfb2b89e4699fc767f2
Jan Zubac <jan.zubac@devoteam.com>
2023-04-19 17:26:28 +0200

    feature(voice-to-text): magic

---
+++ subtitle-maker-from-voice/magic.py
@@ -0,0 +1,19 @@
+import whisper
+from datetime import timedelta
+import os
+
+model = whisper.load_model("base")
+
+transcribe = model.transcribe("data/audio.mp3")
+segments = transcribe['segments']
+
+for segment in segments:
+    startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+',000'
+    endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+',000'
+    text = segment['text']
+    segmentId = segment['id']+1
+    segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
+
+    srtFilename = os.path.join("out", "subs.srt")
+    with open(srtFilename, 'a', encoding='utf-8') as srtFile:
+        srtFile.write(segment)
