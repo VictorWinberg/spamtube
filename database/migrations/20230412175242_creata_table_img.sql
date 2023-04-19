1e0db6061764a50ea88f028b8df98ee6cd734b06
Anton Björkman <anton.bjorkman@enfogroup.com>
2023-04-19 17:56:28 +0200

    review: add goose up & down

--- database/migrations/20230412175242_creata_table_img.sql
+++ database/migrations/20230412175242_creata_table_img.sql
@@ -1,3 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS img (
   url      VARCHAR(255)  NOT NULL UNIQUE
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS img CASCADE;
+-- +goose StatementEnd
