d5bad0391e86e94e93ceab47e0abfb6a1036d271
Jan Zubac <jan.zubac@devoteam.com>
2023-04-19 17:46:40 +0200

    ship it

--- image-uploader/index.js
+++ image-uploader/index.js
@@ -1,59 +1,56 @@
-import fs from 'fs'
+import fs from "fs";
 import path from "path";
-import request from 'request';
-import isImage from 'is-image';
+import request from "request";
+import isImage from "is-image";
 import { config } from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 config({ path: path.resolve(".env.local"), override: true });
-console.log("Saving images to imgbb & spamtube database")
+console.log("Saving images to imgbb & spamtube database");
 async function main() {
+  const API_KEY = process.env.IMGBB_API_KEY;
 
-  const API_KEY = process.env.IMGBB_API_KEY
-
-  let base64ImageArray = []
-  const folderPath = './data'
+  let base64ImageArray = [];
+  const folderPath = "./data";
   // Read files
-  const files = fs.readdirSync(folderPath)
+  const files = fs.readdirSync(folderPath);
   for (const file of files) {
     // Check if file is img? else continue
-    const fullPath = folderPath + '/' + file
-    if (!isImage(fullPath))
-      continue;
+    const fullPath = folderPath + "/" + file;
+    if (!isImage(fullPath)) continue;
 
     // Read file
-    const data = fs.readFileSync(fullPath)
+    const data = fs.readFileSync(fullPath);
     // Buffer to base64
-    const base64String = data.toString('base64');
+    const base64String = data.toString("base64");
     // Add to array of images
     base64ImageArray.push({ base64: base64String, name: file });
   }
-  // Upload to db 
+  // Upload to db
   let index = 1;
   const length = base64ImageArray.length;
   for (const item of base64ImageArray) {
-    console.log(`${index} out of ${length}`)
+    console.log(`${index} out of ${length}`);
     let options = {
-      'method': 'POST',
-      'url': `https://api.imgbb.com/1/upload?name=${item.name}&key=${API_KEY}`,
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded'
+      method: "POST",
+      url: `https://api.imgbb.com/1/upload?name=${item.name}&key=${API_KEY}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       formData: {
-        'image': item.base64
-      }
+        image: item.base64,
+      },
     };
     request(options, async function (error, response) {
       if (error) throw new Error(error);
-      var viewUrl = response.body.data.display_url
+      var viewUrl = response.body.data.display_url;
 
       options = {
-        'method': 'POST',
-        'url': '/api/img', // Will this work? 
-        'headers': {
-          'Content-Type': 'application/json'
+        method: "POST",
+        url: "https://spamtube.zolly.ml/api/img",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ Url: viewUrl })
-
+        body: JSON.stringify({ Url: viewUrl }),
       };
       request(options, function (error, response) {
         if (error) throw new Error(error);
@@ -63,8 +60,4 @@ async function main() {
   }
 }
 
-
 main();
-
-
-
