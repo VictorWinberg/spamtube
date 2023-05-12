0037809d1b70df1f91c2ec0f66d2e24ac6b4b343
Victor Winberg <victor.m.winberg@gmail.com>
2023-05-12 09:31:49 +0200

    disable subs

--- subtitle-maker-from-text/subs.py
+++ subtitle-maker-from-text/subs.py
@@ -15,16 +15,20 @@ def format_time(s: float) -> str:
 
 
 with open('out/subs.srt' , 'w') as f:
-    while (len(words) > 0):
-        sentence = words[:8]
-        words = words[8:]
+    f.write("")
 
-        chars = sum([len(w) for w in sentence])
+print(f'Subtitles disabled')
 
-        f.write(str(counter) + '\n' + format_time(s))
-        s += chars / speed
-        f.write(' --> ' + format_time(s) + '\n' + " ".join(sentence) + '\n\n')
-
-        counter += 1
-
-print(f'Successfully generated subtitles with {counter} lines')
+#    while (len(words) > 0):
+#        sentence = words[:8]
+#        words = words[8:]
+#
+#        chars = sum([len(w) for w in sentence])
+#
+#        f.write(str(counter) + '\n' + format_time(s))
+#        s += chars / speed
+#        f.write(' --> ' + format_time(s) + '\n' + " ".join(sentence) + '\n\n')
+#
+#        counter += 1
+#
+#print(f'Successfully generated subtitles with {counter} lines')
